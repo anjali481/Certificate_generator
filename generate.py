c572bdc2983380905e3ee6f8f4e559c15dd60ca7
pal-16 <mantrypalak@gmail.com>
2021-01-24 20:00:11 +0530

    certificates folder updated

--- generate.py
+++ generate.py
@@ -11,13 +11,13 @@ from PIL import Image, ImageDraw, ImageFont
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
-password ="######" #Your Password
-fromaddr = "#####" #Your Email
+password = "" #Your Password
+fromaddr = "" #Your Email
 df = pd.read_excel('sample-excel-sheet.xlsx')   # Your excel sheet with Participants Name and Email
 
 
 for i in df.index:
-	image = Image.open('########.jpg')           #Your certficate template
+	image = Image.open('.jpg')           #Your certficate template
 	draw = ImageDraw.Draw(image)
 	font = ImageFont.truetype('fonts/GoogleSans-Regular.ttf', size=50)
 	color = 'rgb(0, 230, 118)'
@@ -25,22 +25,20 @@ for i in df.index:
 	name.upper()
 	print(i+1,name)
 	length=len(name)
-	x=1056/2-(length*25)/2   #Here I have calcualted the coordinates of the name to be placed in the certificate assuming each alphabet taking 25 pixel and the certificate width is 1056 pixel
+	x=1056/2-(length*25)/2   #Here I have calculated the coordinates of the name to be placed in the certificate assuming each alphabet taking 25 pixel and the certificate width is 1056 pixel
 	print(x)
 	draw.text((x, 300), name, fill=color, font=font)	
-	temp="sample"
-	imageName = "certificates/"+temp+".pdf"
+	imageName = "certificates/"+name+".pdf"
 	image.save(imageName)
 	
 	toaddr = df['Email'][i]
 	msg = MIMEMultipart() 
-	msg['From'] = "#####"   #Your Email Id 
+	msg['From'] = ""   #Your Email Id 
 	msg['Subject'] = "Certificate for Completing your Android Study Jam"
-	body = '''Please find attached your certificate for completion of the Kotlin Course powered by Anrdoid.
+	body = '''Congratulations! Please find attached your certificate for completion of the Kotlin Course powered by Android. 
 			  
 	Regards
-	DSC 
-			'''
+	DSC '''
 
 	msg.attach(MIMEText(body, 'plain')) 
 
@@ -60,4 +58,3 @@ for i in df.index:
 	s.sendmail(fromaddr, toaddr, text) 
 
 s.quit() 
- 
